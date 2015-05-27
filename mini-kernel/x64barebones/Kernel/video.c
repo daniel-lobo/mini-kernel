8b2fa5cc086e7da1076980316b49bd68078ffcb9
Daniel Lobo <daniel.lobo.182@gmail.com>
2015-05-27 14:50:50 -0300

    modified the start of video driver and created new macros

--- mini-kernel/x64barebones/Kernel/video.c
+++ mini-kernel/x64barebones/Kernel/video.c
@@ -3,17 +3,10 @@
 
 void k_clear_screen(){
     char * vidmem_address = (char *) VIDMEM_ADDRESS;
-    unsigned int i=0;
-    while(i < UPPER_SCREEN_SIZE-LINE_SIZE){
-        vidmem_address[i++]=' ';
-        vidmem_address[i++]=WHITE_TXT;
-    };
-    while(i<UPPER_SCREEN_SIZE){
-        vidmem_address[i++]=' ';
-        vidmem_address[i++]=LILAC_SQUARE;
-    }
+    unsigned int i = 0;
+
     while(i < MAIN_SCREEN_SIZE){
-        vidmem_address[i++]=' ';
-        vidmem_address[i++]=WHITE_TXT;
+        vidmem_address[i++] = ' ';
+        vidmem_address[i++] = WHITE_TXT;
     };
 }
